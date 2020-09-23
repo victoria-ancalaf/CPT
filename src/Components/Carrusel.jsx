df5a81de69299b00d4b546c0f5bbdf38bd876b71
marceortega <marcela.ortegao@mayor.cl>
2020-09-23 20:02:40 -0300

    ubicacion de header, imagen carusel

---
+++ src/Components/Carrusel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carrusel = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Carrusel
---
+++ src/Components/Numeros.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Numeros = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Numeros
