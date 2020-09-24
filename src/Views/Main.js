d53517a709e32327f74c465e7b63837f81b21f6d
adarleika <adarleika@gmail.com>
2020-09-24 19:46:58 -0300

    add footer

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const Main = () => {
     return (
         <div>
             <Navbar />
+            <Footer />
         </div>
     )
 }
