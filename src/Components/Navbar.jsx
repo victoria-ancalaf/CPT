f9fd545bfc5190f4ff2a61111b07a94d92e50f6d
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-09-25 17:55:21 -0300

    adding icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,9 +6,9 @@ import LogoCPT from '../img/comida-para-todos-logo.png';
 
 
 const Navbar = () => {
-
     const [navbar, setNavbar] = useState(false);
-   
+    const [navLinks, setNavLinks] = useState(false);
+
     const changeBackground = () => {
         if(window.scrollY >= 80) {
             setNavbar(true)
@@ -19,18 +19,29 @@ const Navbar = () => {
 
     window.addEventListener('scroll', changeBackground);
 
+    const changeColor = () => {
+        if(window.scrollY >= 80) {
+            setNavLinks(true)
+        } else {
+            setNavLinks(false);
+        }
+    }
+
+    window.addEventListener('scroll', changeColor);
+
+
     return (
         <div>
             <header
                 className={navbar ? 'navbar active' : 'navbar'}>
                 <div className="containerNavbar">
                    <img src={LogoCPT} alt="Comida para todos" /> 
-                    <ul className="nav-links">
-                        <li>Categoría1</li>
-                        <li>Categoría2</li>
-                        <li>Categoría3</li>
-                        <li>Categoría4</li>
-                        <li>Categoría5</li>
+                    <ul className={navLinks ? 'navLinks active' : 'navLinks'}>
+                        <li>Nosotros</li>
+                        <li>Cómo Ayudar</li>
+                        <li>Acción 2030</li>
+                        <li>Portal Colaboradores</li>
+                        <li>Contacto</li>
                     </ul>
                     <button className="btn-donations"> 
                         DONACIONES
--- src/Components/Numbers.jsx
+++ src/Components/Numbers.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import prueba from '../img/iconOperaciones.png'
+import Plato from '../img/Plato_Corazon.svg'
+import Restaurant from '../img/Restaurant.svg'
+import Persona from '../img/Eating.svg'
+import Hands from '../img/Trabajo.svg'
 import '../Styles/Numbers.css'
 
 const Numbers = () => {
@@ -9,22 +12,22 @@ const Numbers = () => {
                 <p className="number-content">Esto es posible gracias a ti</p>
             <div className="icon-container">
                 <div className="icon-text">
-                    <img src={prueba} alt="prueba" />
+                    <img src={Plato} alt="prueba" />
                     <p className="numb">160.000</p>
                     <p className="text">Almuerzos solidarios <br></br>recaudados</p>
                 </div>
-                <div>
-                <img src={prueba} alt="prueba" />
+                <div className="icon-text">
+                <img src={Restaurant} alt="prueba" />
                     <p className="numb">19</p>
                     <p className="text">Restaurantes <br></br>reactivados</p>
                 </div>
-                <div>
-                <img src={prueba} alt="prueba" />
+                <div className="icon-text">
+                <img src={Persona} alt="prueba" />
                     <p className="numb">3.200</p>
                     <p className="text">Personas pueden comer <br></br>a diario</p>
                 </div>
-                <div>
-                <img src={prueba} alt="prueba" />
+                <div className="icon-text">
+                <img src={Hands} alt="prueba" />
                     <p className="numb">72</p>
                     <p className="text">Puestos de empleo <br></br>activados</p>
                 </div>
