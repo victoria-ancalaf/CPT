27166b60acd66ee5b72c8e6a136dfa4f96cf1ece
adarleika <adarleika@gmail.com>
2020-09-28 18:30:13 -0300

    add components fomentamos and add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,52 +1,55 @@
 import React from 'react';
-
+import logoNaranjo from '../img/logoNaranjo.png';
+import '../Styles/Footer.css'
 
 
 
 const Footer = () => {
     return (
-        <div className="container px-lg-2  pt-2 mt-2 pb-2 mb-2">
+        <div className="containerFooter px-lg-2  pt-2 mt-2 pb-2 mb-2">
             <div className="footer-meddle">
                 <div className="main-container px-lg-5 pt-5 mt-5 pb-5 mb-5">
                     <div className="row">
                         {/*colum 1*/}
                         <div className="col-md-3 col-sm-6">
-                            <h4>Lorem ipsum </h4>
                             <ul className="list-unstyled">
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
+                                <img
+                                    className="d-block w-100"
+                                    src={logoNaranjo}
+                                />
+                                <li>Intagram</li>
+                                <li>Correo</li>
+                                <li>Whatsapp</li>
                             </ul>
                         </div>
                         {/*colum 2*/}
                         <div className="col-md-3 col-sm-6">
-                            <h4>Lorem ipsum </h4>
+                            <h4>Red de Comunicaciòn</h4>
                             <ul className="list-unstyled">
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
+                                <li>Ñam</li>
+                                <li>Agencia Land</li>
+                                <li>Estudio Enoki</li>
+                                <li>EcoPass</li>
+                                <li>Tele13 Radio</li>
                             </ul>
                         </div>
                         {/*colum 3*/}
                         <div className="col-md-3 col-sm-6">
-                            <h4>Lorem ipsum </h4>
+                            <h4>Red de Colaboradores Sociales</h4>
                             <ul className="list-unstyled">
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
+                                <li>Las Condenadas</li>
+                                <li>La Chimba</li>
+                                <li>Nosso Prato</li>
                             </ul>
                         </div>
                         {/*colum 4*/}
                         <div className="col-md-3 col-sm-6">
-                            <h4>Lorem ipsum </h4>
+                            <h4>Organizaciones Coordinadoras</h4>
                             <ul className="list-unstyled">
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
-                                <li>lorem ipsum</li>
+                                <li>Cultiva</li>
+                                <li>Gastronomia Social</li>
+                                <li>Colab UC</li>
+                                <li>Social Gastronomy</li>
                             </ul>
                         </div>
                     </div>
@@ -55,7 +58,6 @@ const Footer = () => {
                         <p className="text-xs-center py-3">
                             &copy;{new Date().getFullYear()} Comida para todos -
                         Todos los derechos reservados
-
                     </p>
                     </div>
                 </div>
--- src/Views/Main.js
+++ src/Views/Main.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Carrusel from '../Components/Carrusel'
-import Graficos from '../Components/Graficos'
+//import Graficos from '../Components/Graficos'
 import Operaciones from '../Components/Operaciones'
 import Voluntariado from '../Components/Voluntariado'
 import Numbers from '../Components/Numbers'
 import Colaboradores from '../Components/Colaboradores'
+import Fomentamos from '../Components/Fomentamos'
 
 
 const Main = () => {
@@ -16,8 +17,9 @@ const Main = () => {
             <Carrusel />
             <Voluntariado />
             <Numbers />
+            <Fomentamos />
             <Operaciones />
-            <Graficos />
+            {/*<Graficos />*/}
             <Colaboradores />
             <Footer />
         </div>
