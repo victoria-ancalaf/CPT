206132da9a19586dd9e389f77bfbfdd125c7e0b0
adarleika <adarleika@gmail.com>
2020-09-29 10:01:22 -0300

    footer.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,45 +17,45 @@ const Footer = () => {
                                     className="d-block w-100"
                                     src={logoNaranjo}
                                 />
-                                <li>Intagram</li>
-                                <li>Correo</li>
-                                <li>Whatsapp</li>
+                                <li className="liname">Intagram</li>
+                                <li className="liname">Correo</li>
+                                <li className="liname">Whatsapp</li>
                             </ul>
                         </div>
                         {/*colum 2*/}
                         <div className="col-md-3 col-sm-6">
                             <h4>Red de Comunicaciòn</h4>
                             <ul className="list-unstyled">
-                                <li>Ñam</li>
-                                <li>Agencia Land</li>
-                                <li>Estudio Enoki</li>
-                                <li>EcoPass</li>
-                                <li>Tele13 Radio</li>
+                                <li className="liname">Ñam</li>
+                                <li className="liname">Agencia Land</li>
+                                <li className="liname">Estudio Enoki</li>
+                                <li className="liname">EcoPass</li>
+                                <li className="liname">Tele13 Radio</li>
                             </ul>
                         </div>
                         {/*colum 3*/}
                         <div className="col-md-3 col-sm-6">
                             <h4>Red de Colaboradores Sociales</h4>
                             <ul className="list-unstyled">
-                                <li>Las Condenadas</li>
-                                <li>La Chimba</li>
-                                <li>Nosso Prato</li>
+                                <li className="liname">Las Condenadas</li>
+                                <li className="liname">La Chimba</li>
+                                <li className="liname">Nosso Prato</li>
                             </ul>
                         </div>
                         {/*colum 4*/}
                         <div className="col-md-3 col-sm-6">
                             <h4>Organizaciones Coordinadoras</h4>
                             <ul className="list-unstyled">
-                                <li>Cultiva</li>
-                                <li>Gastronomia Social</li>
-                                <li>Colab UC</li>
-                                <li>Social Gastronomy</li>
+                                <li className="liname">Cultiva</li>
+                                <li className="liname">Gastronomia Social</li>
+                                <li className="liname">Colab UC</li>
+                                <li className="liname">Social Gastronomy</li>
                             </ul>
                         </div>
                     </div>
                     {/*Footer Bottom*/}
                     <div className="footer-copyright text-center py-3">
-                        <p className="text-xs-center py-3">
+                        <p className="parrafo text-xs-center py-3">
                             &copy;{new Date().getFullYear()} Comida para todos -
                         Todos los derechos reservados
                     </p>
