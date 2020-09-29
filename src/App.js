44630b1f82eb01100e98e26bfaf157a08f520621
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-09-29 09:55:49 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import React from 'react';
-
 import Main from '../src/Views/Main'
+import About from '../src/Views/About'
+import Howtohelp from '../src/Views/Howtohelp'
+import Action2030 from '../src/Views/Action2030'
+import Partners from '../src/Views/Partners'
+import Contact from '../src/Views/Contact'
+import Administrator from '../src/Views/Administrator'
+import Forms from '../src/Views/Forms'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-   
-    </div>
-
-
+    <Router>
+      <Switch>
+      <Route path="/Formularios">
+         <Forms />
+        </Route>
+        <Route path="/Administrador">
+         <Administrator />
+        </Route>
+        <Route path="/Contacto">
+         <Contact />
+        </Route>
+        <Route path="/PortalColaboradores">
+         <Partners />
+        </Route>
+        <Route path="/Accion2030">
+         <Action2030 />
+        </Route>
+        <Route path="/ComoAyudar">
+         <Howtohelp />
+        </Route>
+        <Route path="/Nosotros">
+        <About />
+        </Route>
+        <Route exact path="/">
+        <Main />
+        </Route>
+      </Switch>
+  </Router>
   );
 }
 
---
+++ src/Views/Partners.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Partners = () => {
+    return (
+        <div>
+            <h1>Portal Colaboradores</h1>
+        </div>
+    )
+}
+
+export default Partners
