10e1994b8c0ab8117ecddbe62c2ef47f90b961f9
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-09-29 19:03:28 -0300

    Firebase install

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAryiEKpbOjZYDlbN8DRMQQvWGyccdbK7c",
+    authDomain: "comida-para-todos-644ce.firebaseapp.com",
+    databaseURL: "https://comida-para-todos-644ce.firebaseio.com",
+    projectId: "comida-para-todos-644ce",
+    storageBucket: "comida-para-todos-644ce.appspot.com",
+    messagingSenderId: "745492828204",
+    appId: "1:745492828204:web:fdd184c83a82fa0c4d0189"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export { firebase }
