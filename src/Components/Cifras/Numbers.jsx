316b21da3c3e16b968193bc97652b0324d0d973e
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-10-08 19:51:30 -0300

    update cifras/voluntariado

--- src/Components/Cifras/Numbers.jsx
+++ src/Components/Cifras/Numbers.jsx
@@ -8,6 +8,7 @@ import './Numbers.css';
 import './CountUpNumber';
 import CountUpNumber from './CountUpNumber';
 
+
 const Numbers = () => {
 	return (
 		<div className="containerNumbers">
@@ -26,21 +27,25 @@ const Numbers = () => {
 				<div className="icon-text">
 					<img src={Olla} alt="Comedores" />
 					<CountUpNumber className="count" end={28} duration={1.5} />
+						<p className="p-cifras-icon">Comedores <br></br>apoyados</p> 
 				</div>
 				<div className="icon-text">
 				<img src={Chef} alt="Puestos empleo" />
 					<CountUpNumber className="count" end={72} duration={1.5} />
+					<p className="p-cifras-icon">Puestos de empleo<br></br>activados</p>
 				</div>
 				<div className="icon-text">
 				<img src={Comida} alt="Comen a diario" />
 					<CountUpNumber className="count" end={3.2} decimals={3} duration={1.5} />
+					<p className="p-cifras-icon">Personas comen a<br></br>diario</p>
 				</div>
 				<div className="icon-text">
 				<img src={Plato} alt="Almuerzos solidarios" />
 					<CountUpNumber className="count" end={160} decimals={3} duration={1.5} />
+					<p className="p-cifras-icon">Almuerzos solidarios<br></br>recaudados</p>
 				</div>
 			</div>
-			
+		
 					<img src={Vectores} className="vectores" alt='Cifras CPT'></img>
 		
 		</div>
