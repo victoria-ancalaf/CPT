69ce0cef3026731a35a8a8e1ce4b7d8337724800
adarleika <adarleika@gmail.com>
2020-10-08 23:26:26 -0300

    footer listo nuevos cambios

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -70,74 +70,76 @@ const Footer = () => {
                     </div>
 
                 </div>
-
-                <div className="contenedorContenedor">
-                        <div className="contenedorRestaurante">
-                            <ul className="list-unstyled">
-                                <h4>Red de Restaurantes</h4>
-                                {/*<ul className="list-unstyled">*/}
-                                <li className="liname">Boa Santiago</li>
-                                <li className="liname">99 Restaurante</li>
-                                <li className="liname">La Cava del Sommelier</li>
-                                <li className="liname">Aurora Cocina</li>
-                                <li className="liname">Rossonero</li>
-                                <li className="liname">Foodies</li>
-                                <li className="liname">La Caperucita y El Lobo</li>
-                                <li className="liname">Pebre Chile</li>
-                                <li className="liname">Ambrosia Bistro</li>
-                                <li className="liname">Cafè San Juan</li>
-                                <li className="liname">Puerto Claro Valpo</li>
-                                <li className="liname">Taller Macera</li>
-                                <li className="liname">Comedor Comùn</li>
-                            </ul>
-                        </div>
-                        <div className="contenedorMuni">
-                            <ul className="list-unstyled">
-                                <h4>Red de Municipios</h4>
+                <div className="main-container px-lg-4 pt-5 pb-6">
+                    <div className="contenedorrow row">
+                        <div className="contenedorContenedor">
+                            <div className="contenedorRestaurante">
                                 <ul className="list-unstyled">
-                                    <li className="liname">Municipalidad de la Pintana</li>
-                                    <li className="liname">Municipalidad de Antofagasta</li>
+                                    <h4>Red de Restaurantes</h4>
+                                    {/*<ul className="list-unstyled">*/}
+                                    <li className="liname">Boa Santiago</li>
+                                    <li className="liname">99 Restaurante</li>
+                                    <li className="liname">La Cava del Sommelier</li>
+                                    <li className="liname">Aurora Cocina</li>
+                                    <li className="liname">Rossonero</li>
+                                    <li className="liname">Foodies</li>
+                                    <li className="liname">La Caperucita y El Lobo</li>
+                                    <li className="liname">Pebre Chile</li>
+                                    <li className="liname">Ambrosia Bistro</li>
+                                    <li className="liname">Cafè San Juan</li>
+                                    <li className="liname">Puerto Claro Valpo</li>
+                                    <li className="liname">Taller Macera</li>
+                                    <li className="liname">Comedor Comùn</li>
                                 </ul>
-                            </ul>
+                            </div>
                         </div>
-                        <div className="contenedorComunicacion">
-                            {/*colum 3*/}
-                            <h4>Red de Comunicaciòn</h4>
-                            <ul className="list-unstyled">
-                                <li className="liname">Ñam</li>
-                                <li className="liname">Agencia Land</li>
-                                <li className="liname">Estudio Enoki</li>
-                                <li className="liname">EcoPass</li>
-                                <li className="liname">Tele13 Radio</li>
-                            </ul>
-                        </div>
-                        <div className="contenedorSociales">
-                            <h4>Red de Colaboradores Sociales</h4>
-                            <ul className="list-unstyled">
-                                <li className="liname">Las Condenadas</li>
-                                <li className="liname">La Chimba</li>
-                                <li className="liname">Nosso Prato</li>
-                            </ul>
-                        </div>
-                        <div className="contenedorCordinadoras">
-                            <h4>Organizaciones Coordinadoras</h4>
-                            <ul className="list-unstyled">
-                                <li className="liname">Cultiva</li>
-                                <li className="liname">Gastronomia Social</li>
-                                <li className="liname">Colab UC</li>
-                                <li className="liname">Social Gastronomy</li>
-                            </ul>
+                            <div className="contenedorMuni">
+                                <ul className="list-unstyled">
+                                    <h4>Red de Municipios</h4>
+                                    <ul className="list-unstyled">
+                                        <li className="liname">Municipalidad de la Pintana</li>
+                                        <li className="liname">Municipalidad de Antofagasta</li>
+                                    </ul>
+                                </ul>
+                            </div>
+                            <div className="contenedorComunicacion">
+                                {/*colum 3*/}
+                                <h4>Red de Comunicaciòn</h4>
+                                <ul className="list-unstyled">
+                                    <li className="liname">Ñam</li>
+                                    <li className="liname">Agencia Land</li>
+                                    <li className="liname">Estudio Enoki</li>
+                                    <li className="liname">EcoPass</li>
+                                    <li className="liname">Tele13 Radio</li>
+                                </ul>
+                            </div>
+                            <div className="contenedorSociales">
+                                <h4>Red de Colaboradores Sociales</h4>
+                                <ul className="list-unstyled">
+                                    <li className="liname">Las Condenadas</li>
+                                    <li className="liname">La Chimba</li>
+                                    <li className="liname">Nosso Prato</li>
+                                </ul>
+                            </div>
+                            <div className="contenedorCordinadoras">
+                                <h4>Organizaciones Coordinadoras</h4>
+                                <ul className="list-unstyled">
+                                    <li className="liname">Cultiva</li>
+                                    <li className="liname">Gastronomia Social</li>
+                                    <li className="liname">Colab UC</li>
+                                    <li className="liname">Social Gastronomy</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
+                        {/*Footer Bottom*/}
 
-                {/*Footer Bottom*/}
-
-                <p className="parrafo text-xs-center py-3">
-                    &copy;{new Date().getFullYear()} Comida para todos -
-                        Todos los derechos reservados
+                        <p className="parrafo text-xs-center py-3">
+                            &copy;{new Date().getFullYear()} Comida para todos -
+                        Todos los derechos reservados | Ingresar como Administrador
                     </p>
-            </div>
-        </div>
+                    </div>
+                </div>
 
     );
 }
