6f534959d820359d442304e580564737ee52ce41
adarleika <adarleika@gmail.com>
2020-10-13 21:17:37 -0300

    update carousel modelo de operaciones, componente fomentamos

--- src/Components/Fomentamos/Fomentamos.jsx
+++ src/Components/Fomentamos/Fomentamos.jsx
@@ -7,15 +7,15 @@ const Fomentamos = () => {
 
         <div className="containerFomentamos px-lg-2  pt-2 pb-2">
             <div className="footer-meddle">
-                <div className="contenedorFomentamos px-lg-5 pt-5 pb-5 mb-5">
+                <div className="contenedorFomentamos pt-5 pb-5 mb-5">
                     <div className="row">
                         {/*colum 1*/}
-                        <div className="col-md-6 col-sm-6 pt-5">
+                        <div className="claseFomentamos col-md-6 col-sm-6 pt-5">
                             <div className="fomentamos">
-                            <h1>¿Qué <strong> fomentamos? </strong></h1>
+                            <h1 className="tituloFomentamos">¿Qué <strong> fomentamos? </strong></h1>
                             </div>
                             <ul className="list-unstyled">
-                                <li className="liname">En Comida Para Todos trabajamos para proteger
+                                <li className="parrafoFomentamos">En Comida Para Todos trabajamos para proteger
                                 la Seguridad Alimentaria,
                                 generando oportunidades mediante cuatro aristas:
                                 el alimento bueno,
